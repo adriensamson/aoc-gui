d6663c9ce022286deef011565383aa0c68ba52de
Adrien Samson <adrien@adriensamson.fr>
2025-11-24 18:56:46 +0100

    run and display

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Debug)]
 pub enum AocMessage {
     Log(String),
     Progress(usize, usize),
@@ -5,7 +6,7 @@ pub enum AocMessage {
     ResultPart2(String),
 }
 
-pub type AocIterator = dyn Iterator<Item=AocMessage>;
+pub type AocIterator = dyn Iterator<Item=AocMessage> + Send + 'static;
 type AocDay = fn(String) -> Box<AocIterator>;
 
 pub const DAYS : [AocDay; 12] = [
@@ -23,6 +24,15 @@ pub const DAYS : [AocDay; 12] = [
     dummy_day,
 ];
 
-fn dummy_day(_input: String) -> Box<AocIterator> {
-    todo!()
+fn dummy_day(input: String) -> Box<AocIterator> {
+    let line_count = input.lines().count();
+    let cols_count = input.lines().map(|line| line.len()).max().unwrap();
+    Box::new(vec![
+        AocMessage::Log(format!("lines: {line_count}")),
+        AocMessage::Progress(1, 2),
+        AocMessage::ResultPart1(format!("{line_count}")),
+        AocMessage::Log(format!("cols: {cols_count}")),
+        AocMessage::Progress(2, 2),
+        AocMessage::ResultPart2(format!("{cols_count}")),
+    ].into_iter())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::fmt::{Display, Formatter};
+use iced::{widget, Element, Task};
+use iced::futures::Stream;
 use iced::widget::{column};
+use aoc_gui::AocMessage;
 
 fn main() -> iced::Result {
-    iced::application("AOC 2026", App::update, App::view)
+    iced::application("AOC 2025", App::update, App::view)
         .centered()
         .run()
 }
@@ -33,9 +36,10 @@ const DAY_NAMES: [DayName; 12] = [
 #[derive(Default)]
 struct App {
     day: Option<DayName>,
-    input: iced::widget::text_editor::Content,
-    running: Option<Box<aoc_gui::AocIterator>>,
+    input: widget::text_editor::Content,
+    running: bool,
     logs: Vec<String>,
+    progress: (usize, usize),
     result_part1: Option<String>,
     result_part2: Option<String>,
 }
@@ -44,38 +48,81 @@ struct App {
 enum AppMessage {
     Reset,
     SelectDay(DayName),
-    InputAction(iced::widget::text_editor::Action),
+    InputAction(widget::text_editor::Action),
     Run,
+    AocMessage(AocMessage),
 }
 
 impl App {
-    fn update(&mut self, message: AppMessage) {
+    fn update(&mut self, message: AppMessage) -> Task<AppMessage> {
         match message {
             AppMessage::Reset => {
-                self.running = None;
+                self.running = false;
                 self.logs = vec![];
                 self.result_part1 = None;
                 self.result_part2 = None;
+                self.progress = (0, 0);
+                Task::none()
             },
             AppMessage::SelectDay(day) => {
                 self.day = Some(day);
+                Task::none()
             },
             AppMessage::InputAction(action) => {
                 self.input.perform(action);
+                Task::none()
             }
             AppMessage::Run => {
                 if let Some(day) = self.day.and_then(|d| aoc_gui::DAYS.get(d.0)) {
-                    self.running = Some(day(self.input.text()));
-                    todo!("run iterator")
+                    self.running = true;
+                    Task::run(iter_to_stream(day(self.input.text())), AppMessage::AocMessage)
+                } else {
+                    Task::none()
                 }
-            }
+            },
+            AppMessage::AocMessage(AocMessage::Log(log)) => {
+                self.logs.push(log);
+                Task::none()
+            },
+            AppMessage::AocMessage(AocMessage::Progress(prog, total)) => {
+                self.progress = (prog, total);
+                Task::none()
+            },
+            AppMessage::AocMessage(AocMessage::ResultPart1(res)) => {
+                self.result_part1 = Some(res);
+                Task::none()
+            },
+            AppMessage::AocMessage(AocMessage::ResultPart2(res)) => {
+                self.result_part2 = Some(res);
+                Task::none()
+            },
         }
     }
 
-    fn view(&self) -> iced::Element<'_, AppMessage> {
-        let day = iced::widget::pick_list(DAY_NAMES, self.day, AppMessage::SelectDay);
-        let input = iced::widget::text_editor(&self.input).height(120).on_action(AppMessage::InputAction);
-        let run = iced::widget::button("Run").on_press(AppMessage::Run);
+    fn view(&self) -> Element<'_, AppMessage> {
+        let day = widget::pick_list(DAY_NAMES, self.day, AppMessage::SelectDay);
+        let input = widget::text_editor(&self.input).height(120).on_action(AppMessage::InputAction);
+        let run : Element<_> = if self.running {
+            column![
+                widget::button("Reset").on_press(AppMessage::Reset),
+                widget::progress_bar(0f32..=(self.progress.1 as f32), self.progress.0 as f32),
+                if let Some(res) = &self.result_part1 { widget::text!("Part1: {res}") } else { widget::text("Part1...") },
+                if let Some(res) = &self.result_part2 { widget::text!("Part2: {res}") } else { widget::text("Part2...") },
+                column(self.logs.iter().map(|l| widget::text(l).into())),
+            ].into()
+        } else {
+            widget::button("Run").on_press(AppMessage::Run).into()
+        };
         column![day, input, run].into()
     }
+}
+
+fn iter_to_stream<T: Send + 'static>(iter: impl Iterator<Item=T> + Send + 'static) -> impl Stream<Item=T> + Send {
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+    tokio::task::spawn_blocking(move || {
+        for item in iter {
+            tx.send(item).unwrap();
+        }
+    });
+    tokio_stream::wrappers::UnboundedReceiverStream::new(rx)
 }
